03a1adad48595862cd3b145d447e858d15ac9123
prashant18-code <prashant_kumar@tecnics.com>
2023-03-21 11:58:23 +0530

    Initial commit for a demo app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  posts: any;
+  results: any = [];
+  apiData: any;
+  searchQuery = '';
 
-  constructor() {}
+  constructor(private platform: Platform, public httpClient: HttpClient) {  }
 
+  ngOnInit() {
+    this.httpClient.get('https://jsonplaceholder.typicode.com/posts')
+    .subscribe(data => {
+      console.log('my data: ', data);
+      this.apiData = data;
+      this.posts = data;
+    })
+  }
+  handleChange(ev: any) {
+    this.posts.forEach(element => {
+      if(element.title.toLowerCase().indexOf(ev) > -1) {
+        this.results.push(element);
+      }
+    });
+    this.posts = [...this.results];
+  }
+
+  closeClick() {
+    this.searchQuery= '';
+    this.posts = [...this.apiData];
+  }
 }
